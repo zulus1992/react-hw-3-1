8c55c20fa7e6fce879fe1c3b11b9c40e29bb9841
zulus1992 <zulus1992@gmail.com>
2019-09-10 20:34:03 +0300

    2 files modified

--- withOnlineStatus.jsx
+++ withOnlineStatus.jsx
@@ -3,19 +3,17 @@ import React from "react";
 export function withOnlineStatus(Component) {
   return class extends React.Component {
     state = {
-      isOnline: window.navigator.onLine
+      isOnline: navigator.onLine
     };
 
-    onOnlineChange = (isOnline) => {
-      if (isOnline)
-        this.setState({ isOnline, start: performance.now() });
-      else
-        this.setState({ isOnline, end: performance.now() });
+    onOnlineChange = () => {
+      var isOnline=navigator.onLine;
+     this.setState({ isOnline });
     };
 
     componentDidMount() {
-      window.addEventListener('online', this.onOnlineChange(true));
-      window.addEventListener('offline', this.onOnlineChange(false));
+      window.addEventListener('online', this.onOnlineChange);
+      window.addEventListener('offline', this.onOnlineChange);
     }
 
     componentWillUnmounte() {
@@ -25,7 +23,7 @@ export function withOnlineStatus(Component) {
 
     render() {
       return (
-          <Component {...this.props} isOnline={this.state.isOnline}/>
+          <Component  isOnline={this.state.isOnline}/>
       );
     }
   };
